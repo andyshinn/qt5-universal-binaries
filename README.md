732071a0901d64b1e0456022d7113adad19ba9e3
Andy Shinn <andys@andyshinn.as>
2024-07-30 21:53:04 -0500

    initial build

---
+++ README.md
@@ -0,0 +1,3 @@
+# Qt 5 Universal Binaries
+
+Some pre-compiled universal Qt binaries that I can use for builds on GitHub Actions. See the Releases tab for downloads.
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+QT_VERSION_MAJOR="5.15"
+QT_VERSION="$QT_VERSION_MAJOR.14"
+QT_DOWNLOAD_URL="https://download.qt.io/official_releases/qt/$QT_VERSION_MAJOR/$QT_VERSION/single/qt-everywhere-opensource-src-$QT_VERSION.tar.xz"
+QT_FOLDER="qt-everywhere-opensource-src-$QT_VERSION"
+QT_PREFIX="/usr/local/qt-$QT_VERSION"
+
+export DEVELOPER_DIR="/Applications/Xcode_14.3.1.app/Contents/Developer"
+
+# Download Qt sources
+wget -O qt.tar.xz "$QT_DOWNLOAD_URL"
+tar -xf qt.tar.xz
+
+# Build Qt
+mkdir qt-build
+cd qt-build
+../$QT_FOLDER/configure QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" -opensource -confirm-license -nomake examples -nomake tests -no-openssl -securetransport -prefix "$QT_PREFIX"
+make -j4
+make -j4 install
+
+# Package as tarball
+tar -czf qt-mac-universal.tar.gz "$QT_PREFIX"
